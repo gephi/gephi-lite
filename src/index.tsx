70a76264c18dc88b28a3a0863d5d4a1acb48df82
Paul Girard <paul@ouestware.com>
2023-01-23 17:37:54 +0100

    [i18n] setup i18n - i18n with react-i18next - scanner to collect translation keys from source - examples in home

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LngDetector from "i18next-browser-languagedetector";
+import locales from "./locales";
+
 import reportWebVitals from "./reportWebVitals";
 import { Root } from "./core/Root";
 
 import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+i18n
+  .use(initReactI18next)
+  .use(LngDetector)
+  .init({
+    debug: true,
+    resources: locales,
+    detection: {
+      order: ["querystring", "navigator"],
+      lookupQuerystring: "lng",
+    },
+  });
 
 root.render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/locales/index.ts
@@ -0,0 +1,8 @@
+import en from "./en.json";
+import fr from "./fr.json";
+
+const translations = {
+  en: { translation: en },
+  fr: { translation: fr },
+};
+export default translations;
