9d4ad0044d262f04865c5c79c7fbe5451a5e362f
Alexis Jacomy <alexis.jacomy@gmail.com>
2023-01-27 17:28:40 +0100

    Improves StatisticsIcon

--- src/components/common-icons.tsx
+++ src/components/common-icons.tsx
@@ -1,11 +1,12 @@
 import { TbCircles } from "react-icons/tb";
+import { GrScorecard } from "react-icons/gr";
 import { RiFilterFill } from "react-icons/ri";
 import { ImFilesEmpty } from "react-icons/im";
 import { BiNetworkChart } from "react-icons/bi";
-import { BsSlashLg, BsCircle, BsGithub, BsPalette, BsCodeSlash } from "react-icons/bs";
+import { BsSlashLg, BsCircle, BsGithub, BsPalette } from "react-icons/bs";
 
 export const GraphIcon = BiNetworkChart;
-export const StatisticsIcon = BsCodeSlash;
+export const StatisticsIcon = GrScorecard;
 export const AppearanceIcon = BsPalette;
 export const FiltersIcon = RiFilterFill;
 export const LayoutsIcon = TbCircles;
