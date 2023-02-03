74a2503ba642915bbf7ea8826e002b57faefed7d
Alexis Jacomy <alexis.jacomy@gmail.com>
2023-02-03 11:50:41 +0100

    [metrics] Adds degree metric to collection

--- src/core/metrics/collections.ts
+++ src/core/metrics/collections.ts
@@ -6,6 +6,7 @@ import betweennessCentrality from "graphology-metrics/centrality/betweenness";
 
 import { Metric } from "./types";
 import { DataGraph, EdgeRenderingData } from "../graph/types";
+import { toNumber } from "../utils/casting";
 
 export const NODE_METRICS: Metric<"nodes", any, any>[] = [
   {
@@ -123,6 +124,54 @@ export const NODE_METRICS: Metric<"nodes", any, any>[] = [
           normalized: parameters.normalize,
         }),
       };
+    },
+  },
+  {
+    id: "degree",
+    description: true,
+    types: { degree: "number" },
+    itemType: "nodes",
+    parameters: [
+      {
+        id: "kind",
+        type: "enum",
+        values: [{ id: "degree" }, { id: "inDegree" }, { id: "outDegree" }],
+        defaultValue: "degree",
+      },
+      {
+        id: "getEdgeWeight",
+        type: "attribute",
+        itemType: "edges",
+        restriction: "quantitative",
+      },
+    ],
+    fn(
+      {
+        kind,
+        getEdgeWeight,
+      }: {
+        kind?: "degree" | "inDegree" | "outDegree";
+        getEdgeWeight?: keyof EdgeRenderingData;
+      },
+      graph: DataGraph,
+    ) {
+      const collection: Record<string, number> = {};
+
+      if (getEdgeWeight) {
+        const reduceEdges = (
+          kind === "inDegree" ? graph.reduceInEdges : kind === "outDegree" ? graph.reduceOutEdges : graph.reduceEdges
+        ).bind(graph);
+        graph.forEachNode((node) => {
+          collection[node] = reduceEdges(node, (acc, edge, attr) => acc + (toNumber(attr[getEdgeWeight]) || 0), 0);
+        });
+      } else {
+        const getDegree = graph[kind || "degree"].bind(graph);
+        graph.forEachNode((node) => {
+          collection[node] = getDegree(node);
+        });
+      }
+
+      return { degree: collection };
     },
   },
   {
