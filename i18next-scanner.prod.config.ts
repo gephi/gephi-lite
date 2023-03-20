018dddd041369759b9ebeaedadb6d75d528c3c9c
Benoit Simard <contact@bsimard.com>
2023-03-20 10:42:53 +0100

    Format i18n prod config file

--- i18next-scanner.prod.config.ts
+++ i18next-scanner.prod.config.ts
@@ -1,3 +1,9 @@
 const config = require("./i18next-scanner.dev.config.ts");
 
-module.exports = { options: { ...config.options, lngs: ["en", "fr"], debug: false } };
+module.exports = {
+  options: {
+    ...config.options,
+    lngs: ["en", "fr"],
+    debug: false,
+  },
+};
