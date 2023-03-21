7075c7eebf4c3f07b3af66fce3fb64e441fa6b1d
Paul Girard <paul@ouestware.com>
2023-03-21 14:55:42 +0100

    [Filters] slider with barchart in Range fixes #32

--- src/components/GraphFilters/RangeFilter.tsx
+++ src/components/GraphFilters/RangeFilter.tsx
@@ -1,20 +1,45 @@
-import { flatMap, max, min } from "lodash";
+import { flatMap, isNumber, keyBy, last, mapValues, max, min, uniq } from "lodash";
 import { FC, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import Slider, { SliderProps } from "rc-slider";
+import cx from "classnames";
 
 import { useFiltersActions } from "../../core/context/dataContexts";
 import { RangeFilterType } from "../../core/filters/types";
 import { graphDatasetAtom } from "../../core/graph";
+import { findRanges, shortenNumber } from "./utils";
+
+interface RangeValue {
+  min: number;
+  max: number;
+  values: number[];
+}
+
+interface RangeMetric {
+  unit: number;
+  step: number;
+  min: number;
+  max: number;
+  maxCount: number;
+  ranges: RangeValue[];
+}
+
+const RANGE_STYLE = {
+  dotStyle: { borderColor: "#ccc" },
+  railStyle: { backgroundColor: "#ccc" },
+  activeDotStyle: { borderColor: "black" },
+  trackStyle: [{ backgroundColor: "black" }, { backgroundColor: "black" }],
+  handleStyle: [
+    { backgroundColor: "white", borderColor: "black" },
+    { backgroundColor: "white", borderColor: "black" },
+  ],
+};
 
 export const RangeFilterEditor: FC<{ filter: RangeFilterType }> = ({ filter }) => {
   const { t } = useTranslation();
   const { replaceCurrentFilter } = useFiltersActions();
 
-  const [dataMinMax, setDataMinMax] = useState<{ min?: number; max?: number }>({});
-  const [filterMinMax, setFilterMinMax] = useState<{ min?: number; max?: number }>({
-    min: filter.min,
-    max: filter.max,
-  });
+  const [rangeMetric, setRangeMetric] = useState<RangeMetric>();
 
   useEffect(() => {
     const values = flatMap(
@@ -27,28 +52,121 @@ export const RangeFilterEditor: FC<{ filter: RangeFilterType }> = ({ filter }) =
     ) as number[];
     const minValue = min(values);
     const maxValue = max(values);
-    setDataMinMax({ min: minValue, max: maxValue });
-  }, [filter]);
+    if (minValue && maxValue) {
+      const { unit, ranges } = findRanges(minValue, maxValue);
+      const rangeValues = ranges.map((range) => {
+        const rangeValues = values.filter((v) => v >= range[0] && v < range[1]);
+        return {
+          min: range[0],
+          max: range[1],
+          values: rangeValues,
+        };
+      });
+
+      setRangeMetric({
+        min: ranges[0][0],
+        max: (last(ranges) || ranges[0])[1],
+        step: unit < 1 || unit >= 10 ? unit / 10 : 1,
+        unit,
+        ranges: rangeValues,
+        maxCount: Math.max(...rangeValues.map((r) => r.values.length)),
+      });
+    }
+  }, [filter.itemType, filter.field]);
+
+  const marks: SliderProps["marks"] = rangeMetric
+    ? mapValues(
+        keyBy(
+          uniq(
+            rangeMetric.ranges
+              .flatMap((r) => [r.min, r.max])
+              .concat([filter.min || rangeMetric.min, filter.max || rangeMetric.max]),
+          ),
+        ),
+        (v) =>
+          v === filter.min || v === filter.max
+            ? {
+                label: shortenNumber(v),
+                style: { fontWeight: "bold", background: "white", padding: "0 0.2em", zIndex: 1 },
+              }
+            : { label: " ", style: { fontWeight: "bold", background: "white", padding: "0 0.2em", zIndex: 1 } },
+      )
+    : {};
 
   return (
     <form onSubmit={(e) => e.preventDefault()}>
+      {rangeMetric && (
+        <ul className="list-unstyled range-filter">
+          {(rangeMetric.ranges || []).map((range) => {
+            const globalCount = range.values.length;
+            const filteredCount = range.values.filter(
+              (v) => !filter.min || (v >= filter.min && (!filter.max || v < filter.max)),
+            ).length;
+            const filteredHeight = (filteredCount / rangeMetric.maxCount) * 100;
+            const isLabelInside = filteredHeight > 90;
+
+            return (
+              <div className="bar" key={range.min}>
+                <div className="global" style={{ height: (globalCount / rangeMetric.maxCount) * 100 + "%" }} />
+                <div
+                  className="filtered"
+                  style={{
+                    height: filteredHeight + "%",
+                  }}
+                >
+                  {filteredCount < globalCount && (
+                    <span
+                      className={cx("label", isLabelInside ? "inside" : "outside")}
+                      //style={isLabelInside ? { color: fontColor } : undefined}
+                    >
+                      {shortenNumber(filteredCount)}
+                    </span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+      )}
+
+      <Slider
+        className="pb-4 mb-3"
+        range
+        value={
+          [
+            filter.min !== undefined ? filter.min : rangeMetric?.min,
+            filter.max !== undefined ? filter.max : rangeMetric?.max,
+          ].filter((n) => isNumber(n)) as number[]
+        }
+        {...rangeMetric}
+        marks={marks}
+        onChange={(value) => {
+          if (Array.isArray(value)) {
+            const [minSelected, maxSelected] = value;
+            replaceCurrentFilter({ ...filter, min: minSelected, max: maxSelected });
+          }
+        }}
+        allowCross={false}
+        {...RANGE_STYLE}
+      />
+
       <div className="d-flex">
         <div className="d-flex align-items-center mt-1">
           <input
             className="form-control form-control-sm"
             id="min"
             type="number"
-            min={dataMinMax.min}
-            max={filterMinMax.max}
-            value={filterMinMax.min}
-            placeholder={"" + dataMinMax.min}
+            min={rangeMetric?.min}
+            // max={filter.max}
+            step={rangeMetric?.step}
+            value={filter.min}
+            placeholder={"" + rangeMetric?.min}
             onChange={(e) => {
-              const newMin = e.target.value ? +e.target.value : undefined;
-              setFilterMinMax({ min: newMin, max: filterMinMax.max });
+              const newMin = +e.target.value;
               replaceCurrentFilter({ ...filter, min: newMin });
             }}
           />
-          <label className="form-check-label small ms-1" htmlFor="max">
+          <label className="form-check-label small ms-1" htmlFor="min">
             {t("common.min")}
           </label>
         </div>
@@ -57,13 +175,13 @@ export const RangeFilterEditor: FC<{ filter: RangeFilterType }> = ({ filter }) =
             className="form-control form-control-sm "
             id="max"
             type="number"
-            min={filterMinMax.min}
-            max={dataMinMax.max}
-            placeholder={"" + dataMinMax.max}
-            value={filterMinMax.max}
+            min={filter.min}
+            max={rangeMetric?.max}
+            step={rangeMetric?.step}
+            placeholder={"" + rangeMetric?.max}
+            value={filter.max}
             onChange={(e) => {
-              const newMax = e.target.value ? +e.target.value : undefined;
-              setFilterMinMax({ min: filterMinMax.min, max: newMax });
+              const newMax = +e.target.value;
               replaceCurrentFilter({ ...filter, max: newMax });
             }}
           />
---
+++ src/components/GraphFilters/utils.ts
@@ -0,0 +1,42 @@
+// copied from https://gitlab.com/ouestware/retina/-/blob/main/src/utils/number.ts#L3-20
+import { inRange, round } from "lodash";
+
+export function findRanges(min: number, max: number): { unit: number; ranges: [number, number][] } {
+  if (max <= min) return { ranges: [[Math.min(min, max), Math.max(min, max)]], unit: Math.abs(max - min) };
+
+  const ranges: [number, number][] = [];
+
+  const diff = max - min;
+  const digits = Math.floor(Math.log10(diff)) - 1;
+  const p = Math.pow(10, digits);
+  const unit = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 1000].map((n) => n * p).find((n) => inRange(diff / n, 5, 15));
+
+  if (!unit) return { ranges: [[min, max]], unit: max - min };
+
+  for (let i = Math.floor(min / unit); i <= max / unit; i++) {
+    ranges.push([round(i * unit, -digits), round((i + 1) * unit, -digits)]);
+  }
+
+  return { unit, ranges };
+}
+export function shortenNumber(n: number): string {
+  if (n === 0) return "0";
+  if (n < 0) return "-" + shortenNumber(-n);
+
+  const suffixes = ["", "k", "m", "b", "t"];
+  const suffixNum = Math.floor(Math.log10(n) / 3);
+  const shortValue = suffixNum ? +(n / Math.pow(1000, suffixNum)).toFixed(2) : n;
+
+  return suffixes[suffixNum]
+    ? (shortValue % 1 ? shortValue.toFixed(1) : shortValue) + suffixes[suffixNum]
+    : n.toPrecision(3).replace(/\.?0+$/, "");
+}
+
+export function isNumber(v: unknown): boolean {
+  if (typeof v === "number") return true;
+  if (typeof v === "string") {
+    return !isNaN(+v);
+  }
+
+  return false;
+}
