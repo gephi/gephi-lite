c70d0c998d92917868c315468833200d298def5a
Paul Girard <paul@ouestware.com>
2023-03-22 17:14:24 +0100

    [Appearance] color ranking slider + picker

---
+++ src/components/GraphAppearance/color/ColorPickerTooltip.tsx
@@ -0,0 +1,113 @@
+import { FC, HTMLProps, LegacyRef, useEffect, useRef, useState } from "react";
+import { SketchPicker } from "react-color";
+import TetherComponent from "react-tether";
+
+const ColorPickerTooltip: FC<{
+  id?: string;
+  className?: string;
+  disabled?: boolean;
+  targetProps: HTMLProps<HTMLDivElement>;
+  targetState: {
+    index: number;
+    value: number[];
+    valueNow: number;
+  };
+  value: string;
+  onChange: (newValue: string) => void;
+}> = ({ id, className, disabled, value, onChange, targetProps, targetState }) => {
+  const [color, setColor] = useState(value);
+  const [showPicker, setShowPicker] = useState<boolean>(false);
+  const [isDraggingThumb, setIsDraggingThumb] = useState<boolean>(false);
+
+  const pickerWrapper = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!showPicker) {
+      if (color !== value) onChange(color);
+      return;
+    }
+
+    const handleClickBody = (e: MouseEvent) => {
+      if (pickerWrapper.current && !pickerWrapper.current.contains(e.target as Node)) {
+        setShowPicker(false);
+      }
+    };
+
+    setTimeout(() => document.body.addEventListener("click", handleClickBody), 0);
+    return () => document.body.removeEventListener("click", handleClickBody);
+  }, [showPicker]);
+
+  useEffect(() => {
+    setColor(value);
+  }, [value]);
+
+  return (
+    <TetherComponent
+      //attachment={"top center"}
+      className="over-modal shadow"
+      //constraints={[{ to: "window", attachment: "together", pin: true }]}
+      renderTarget={(ref) => (
+        <div
+          ref={ref}
+          {...targetProps}
+          style={{
+            ...targetProps.style,
+            backgroundColor: value,
+            left:
+              targetState.index === 0
+                ? 0
+                : targetState.index === targetState.value.length - 1
+                ? undefined
+                : targetProps.style?.left,
+            right: targetState.index === targetState.value.length - 1 ? 0 : undefined,
+          }}
+          onClick={(e) => {
+            if (isDraggingThumb === false) {
+              console.log("sho picker");
+              setShowPicker(!showPicker);
+            }
+            e.stopPropagation();
+            e.preventDefault();
+            setIsDraggingThumb(false);
+          }}
+          onMouseMove={(e) => {
+            if (!isDraggingThumb && e.buttons === 1) {
+              if ([0, targetState.value.length - 1].includes(targetState.index)) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+              setIsDraggingThumb(true);
+            }
+          }}
+        />
+      )}
+      renderElement={(ref) =>
+        showPicker && (
+          // We use two divs here to allow having "two refs":
+          <div ref={ref as LegacyRef<HTMLDivElement>}>
+            <div ref={pickerWrapper} className="custom-color-picker">
+              <SketchPicker
+                color={color}
+                onChange={(color) => setColor(color.hex)}
+                styles={{
+                  default: {
+                    picker: {
+                      boxShadow: "none",
+                    },
+                  },
+                }}
+              />
+              <div className="addendum">
+                <div className="form-check">
+                  <button>TODO delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    />
+  );
+};
+
+export default ColorPickerTooltip;
--- src/components/GraphAppearance/color/ColorRankingEditor.tsx
+++ src/components/GraphAppearance/color/ColorRankingEditor.tsx
@@ -1,8 +1,12 @@
-import { FC } from "react";
+import { FC, HTMLProps, useCallback, useMemo, useState } from "react";
 import chroma from "chroma-js";
 import { useTranslation } from "react-i18next";
+import ReactSlider from "react-slider";
+
 import { ItemType } from "../../../core/types";
 import { RankingColor } from "../../../core/appearance/types";
+import { max, sortBy } from "lodash";
+import ColorPickerTooltip from "./ColorPickerTooltip";
 
 export const ColorRankingEditor: FC<{
   itemType: ItemType;
@@ -10,8 +14,95 @@ export const ColorRankingEditor: FC<{
   setColor: (newColor: RankingColor) => void;
 }> = ({ itemType, color, setColor }) => {
   const { t } = useTranslation();
+
+  const values = useMemo(() => color.colorScalePoints.map((m) => m.scalePoint), [color.colorScalePoints]);
+
+  const [isDraggingTrack, setIsDraggingTrack] = useState<boolean>(false);
+
+  const thumbRenderer: (
+    props: HTMLProps<HTMLDivElement>,
+    state: {
+      index: number;
+      value: number[];
+      valueNow: number;
+    },
+  ) => JSX.Element | null = useCallback(
+    (props, state) => (
+      <ColorPickerTooltip
+        value={color.colorScalePoints[state.index].color}
+        targetProps={props}
+        targetState={state}
+        onChange={() => {
+          console.log("TODO");
+        }}
+      />
+    ),
+    [color.colorScalePoints],
+  );
+
   return (
     <>
+      <ReactSlider
+        key={values.length}
+        className="horizontal-slider"
+        thumbClassName="thumb"
+        trackClassName="track"
+        snapDragDisabled
+        renderThumb={thumbRenderer}
+        onSliderClick={(position) => {
+          if (!isDraggingTrack) {
+            setColor({
+              ...color,
+              colorScalePoints: sortBy(
+                color.colorScalePoints.concat({
+                  color: chroma.random().hex(),
+                  scalePoint: position,
+                }),
+                (csp) => csp.scalePoint,
+              ),
+            });
+          }
+          setIsDraggingTrack(false);
+        }}
+        renderTrack={(props, state) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              background:
+                state.index > 0 && state.index < color.colorScalePoints.length
+                  ? `linear-gradient(90deg, ${color.colorScalePoints[state.index - 1].color}, ${
+                      color.colorScalePoints[state.index].color
+                    })`
+                  : undefined,
+              right: max([0, props.style?.right]),
+            }}
+            onMouseMove={(e) => {
+              if (e.buttons === 1) {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsDraggingTrack(true);
+              }
+            }}
+          />
+        )}
+        value={values}
+        min={0.01}
+        max={0.99}
+        step={0.01}
+        pearling
+        onAfterChange={(values) => {
+          if (Array.isArray(values))
+            setColor({
+              ...color,
+              colorScalePoints: color.colorScalePoints.map((csp, i) => ({
+                ...csp,
+                scalePoint: i === 0 ? 0 : i === values.length - 1 ? 1 : values[i],
+              })),
+            });
+        }}
+      />
+
       <div>
         {color.colorScalePoints.map((sc, index) => {
           return (
