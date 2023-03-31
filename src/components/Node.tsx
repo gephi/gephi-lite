b474b8e283b4bc8a9dac4c2055161a040a74148e
Benoit Simard <contact@bsimard.com>
2023-03-31 16:44:25 +0200

    Fix #54

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,11 +1,15 @@
 import { FC, ReactNode } from "react";
+import { useTranslation } from "react-i18next";
 import cx from "classnames";
 
 export const NodeComponent: FC<{ label: ReactNode; color: string; hidden?: boolean }> = ({ label, color, hidden }) => {
+  const { t } = useTranslation();
   return (
     <>
       <span className={hidden ? "circle" : "disc"} style={{ backgroundColor: color }} />{" "}
-      <span className={cx(hidden && "text-muted")}>{label}</span>
+      <span className={cx(hidden && "text-muted", !label && "fst-italic")}>
+        {label || t("selection.node_no_label")}
+      </span>
     </>
   );
 };
