6989db319ca5e6adf958911dffeee768ccc0b453
Benoit Simard <contact@bsimard.com>
2023-07-06 18:31:23 +0200

    Merging GraphSearch and NodeSearch

--- src/components/GraphSearch.tsx
+++ src/components/GraphSearch.tsx
@@ -1,30 +1,28 @@
 import { FC, useCallback } from "react";
 import AsyncSelect from "react-select/async";
-import { OptionProps } from "react-select";
+import { OptionProps, SingleValueProps } from "react-select";
 import cx from "classnames";
 import { debounce } from "lodash";
 import { useTranslation } from "react-i18next";
 
 import { ItemType } from "../core/types";
-import { useSearch, useSelectionActions, useAppearance } from "../core/context/dataContexts";
+import { useSearch, useAppearance } from "../core/context/dataContexts";
 import { SearchIcon } from "./common-icons";
 import { NodeComponentById } from "./Node";
 import { EdgeComponentById } from "./Edge";
 
-interface OptionItem {
+export interface OptionItem {
   id: string;
   type: ItemType;
 }
-interface OptionMessage {
+export interface OptionMessage {
   type: "message";
   i18nCode: string;
   i18nParams: { [key: string]: unknown };
   action?: () => void;
 }
 
-const RESULT_MAX_SIZE = 25;
-
-type Option = OptionItem | OptionMessage;
+export type Option = OptionItem | OptionMessage;
 
 const OptionComponent = ({ data, innerProps, className, isFocused }: OptionProps<Option, false>) => {
   const { t } = useTranslation();
@@ -58,104 +56,71 @@ const IndicatorComponent = () => {
   );
 };
 
+const SingleValue = ({ data }: SingleValueProps<Option>) => {
+  if (data.type === "nodes") return <NodeComponentById id={data.id} />;
+  if (data.type === "edges") return <EdgeComponentById id={data.id} />;
+  return null;
+};
+
+interface GraphSearchProps {
+  className?: string;
+  value?: Option | null;
+  /**
+   * If not specified, we search on nodes & edges
+   */
+  type?: ItemType;
+  /**
+   * What we do when user select an item
+   */
+  onChange: (e: Option | null) => void;
+  /**
+   * With this function, you can filter/enhance the result displayed to the user.
+   * This can useful to add action / messages or limit the number of result
+   */
+  postProcessOptions?: (options: Option[]) => Option[];
+}
+
 /**
  * Search a node/edge
  */
-export const GraphSearch: FC<{ className?: string }> = ({ className }) => {
+export const GraphSearch: FC<GraphSearchProps> = ({ className, onChange, postProcessOptions, type, value }) => {
   const { t } = useTranslation();
   const { index } = useSearch();
-  const { select } = useSelectionActions();
   const { nodesLabel, edgesLabel } = useAppearance();
-
-  /**
-   * What we do when user select an item in the list
-   *  If it's node/edge, we add it to the selection
-   *  If it's an action, we execute it
-   */
-  const onChange = useCallback(
-    (option: Option | null) => {
-      if (option) {
-        if (option.type === "nodes" || option.type === "edges")
-          select({ type: option.type, items: new Set([option.id]) });
-
-        if (option.type === "message" && option.action) option.action();
-      }
-    },
-    [select],
-  );
 
   /**
    * Loading the options while the user is typing.
    */
   const loadOptions = useCallback(
     (query: string, callback: (options: Option[]) => void) => {
-      const searchResult = index.search(query, {
-        prefix: true,
-        fuzzy: 0.2,
-        boost: {
-          ...(nodesLabel.type === "field" ? { [nodesLabel.field]: 2 } : { label: 2 }),
-          ...(edgesLabel.type === "field" ? { [edgesLabel.field]: 2 } : { label: 2 }),
-        },
-      });
-
-      const result: Option[] = searchResult
-        .slice(0, RESULT_MAX_SIZE - 1)
+      const result = index
+        .search(query, {
+          prefix: true,
+          fuzzy: 0.2,
+          filter: type ? (result) => result.type === type : undefined,
+          boost: {
+            ...(nodesLabel.type === "field" ? { [nodesLabel.field]: 2 } : { label: 2 }),
+            ...(edgesLabel.type === "field" ? { [edgesLabel.field]: 2 } : { label: 2 }),
+          },
+        })
         .map((item) => ({ id: item.id, type: item.type }));
 
-      if (searchResult.length > 1) {
-        if (searchResult.length > RESULT_MAX_SIZE) {
-          result.push({
-            type: "message",
-            i18nCode: "other_result",
-            i18nParams: { count: searchResult.length - RESULT_MAX_SIZE },
-          });
-        } else {
-          const nodesResult = result.filter((r): r is OptionItem => r.type === "nodes").map((r) => r.id);
-          if (nodesResult.length > 0) {
-            result.push({
-              type: "message",
-              i18nCode: "select_all_nodes",
-              i18nParams: { count: nodesResult.length },
-              action: () => {
-                select({
-                  type: "nodes",
-                  items: new Set(nodesResult),
-                });
-              },
-            });
-          }
-
-          const edgesResult = result.filter((r): r is OptionItem => r.type === "edges").map((r) => r.id);
-          if (edgesResult.length > 0) {
-            result.push({
-              type: "message",
-              i18nCode: "select_all_edges",
-              i18nParams: { count: edgesResult.length },
-              action: () => {
-                select({
-                  type: "edges",
-                  items: new Set(edgesResult),
-                });
-              },
-            });
-          }
-        }
-      }
-
-      callback(result);
+      callback(postProcessOptions ? postProcessOptions(result) : result);
     },
-    [index, select, nodesLabel, edgesLabel],
+    [index, nodesLabel, edgesLabel, type, postProcessOptions],
   );
 
   return (
     <AsyncSelect<Option>
       className={className}
       isClearable
-      placeholder={t("search.placeholder")}
-      value={null}
+      controlShouldRenderValue={!!value}
+      placeholder={t(`search.${type || "graph"}.placeholder`)}
+      value={value || null}
       loadOptions={debounce(loadOptions, 200)}
       onChange={onChange}
       components={{
+        SingleValue,
         Option: OptionComponent,
         DropdownIndicator: IndicatorComponent,
         NoOptionsMessage: (props) => {
@@ -163,9 +128,9 @@ export const GraphSearch: FC<{ className?: string }> = ({ className }) => {
           return (
             <div className="text-center p-2 text-muted">
               {props.selectProps.inputValue.length > 0 ? (
-                <span>{t("search.no_result")}</span>
+                <span>{t(`search.${type || "graph"}.no_result`)}</span>
               ) : (
-                <span>{t("search.help")}</span>
+                <span>{t(`search.${type || "graph"}.help`)}</span>
               )}
             </div>
           );
@@ -178,6 +143,11 @@ export const GraphSearch: FC<{ className?: string }> = ({ className }) => {
             ...styles,
           };
         },
+        valueContainer: (provided) => ({
+          ...provided,
+          display: "flex",
+          flexWrap: "nowrap",
+        }),
       }}
     />
   );
--- src/views/graphPage/ContextPanel.tsx
+++ src/views/graphPage/ContextPanel.tsx
@@ -1,14 +1,15 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { capitalize } from "lodash";
 import { useTranslation } from "react-i18next";
 import { AiOutlinePlus } from "react-icons/ai";
 
 import { Selection } from "./Selection";
-import { GraphSearch } from "../../components/GraphSearch";
+import { GraphSearch, Option, OptionItem } from "../../components/GraphSearch";
 import { ContextIcon } from "../../components/common-icons";
 import { useFilteredGraph, useGraphDataset } from "../../core/context/dataContexts";
 import { ItemType } from "../../core/types";
 import { useModal } from "../../core/modals";
+import { useSelectionActions } from "../../core/context/dataContexts";
 import UpdateNodeModal from "./modals/edition/UpdateNodeModal";
 import UpdateEdgeModal from "./modals/edition/UpdateEdgeModal";
 
@@ -50,6 +51,76 @@ const GraphStat: FC<{ type: ItemType; current: number; total: number }> = ({ typ
   );
 };
 
+const RESULT_MAX_SIZE = 25;
+
+const GraphSelectionSearch: FC = () => {
+  const { select } = useSelectionActions();
+
+  const onChange = useCallback(
+    (option: Option | null) => {
+      if (option) {
+        if (option.type === "message") {
+          if (option.action) option.action();
+        } else {
+          select({ type: option.type, items: new Set([option.id]) });
+        }
+      }
+    },
+    [select],
+  );
+
+  const postProcessOptions = useCallback(
+    (searchResult: Option[]) => {
+      const result: Option[] = searchResult.slice(0, RESULT_MAX_SIZE - 1);
+
+      if (searchResult.length > 1) {
+        if (searchResult.length > RESULT_MAX_SIZE) {
+          result.push({
+            type: "message",
+            i18nCode: "other_result",
+            i18nParams: { count: searchResult.length - RESULT_MAX_SIZE },
+          });
+        } else {
+          const nodesResult = result.filter((r): r is OptionItem => r.type === "nodes").map((r) => r.id);
+          if (nodesResult.length > 0) {
+            result.push({
+              type: "message",
+              i18nCode: "select_all_nodes",
+              i18nParams: { count: nodesResult.length },
+              action: () => {
+                select({
+                  type: "nodes",
+                  items: new Set(nodesResult),
+                });
+              },
+            });
+          }
+
+          const edgesResult = result.filter((r): r is OptionItem => r.type === "edges").map((r) => r.id);
+          if (edgesResult.length > 0) {
+            result.push({
+              type: "message",
+              i18nCode: "select_all_edges",
+              i18nParams: { count: edgesResult.length },
+              action: () => {
+                select({
+                  type: "edges",
+                  items: new Set(edgesResult),
+                });
+              },
+            });
+          }
+        }
+      }
+
+      return result;
+    },
+    [select],
+  );
+
+  return <GraphSearch value={null} onChange={onChange} postProcessOptions={postProcessOptions} />;
+};
+
 export const ContextPanel: FC = () => {
   const { t } = useTranslation();
   const { fullGraph } = useGraphDataset();
@@ -67,7 +138,7 @@ export const ContextPanel: FC = () => {
       <GraphStat type="edges" current={filteredGraph.size} total={fullGraph.size} />
       <hr />
 
-      <GraphSearch />
+      <GraphSelectionSearch />
       <hr />
 
       <Selection />
