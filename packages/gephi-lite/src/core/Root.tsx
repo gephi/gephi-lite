fadd1d51d2b40b18da3e7ab6eda4d64acec473d3
Arthur Desaintjan <arthur@ouestware.com>
2025-06-23 17:42:01 +0200

    [design system] multipage

--- packages/gephi-lite/src/core/Root.tsx
+++ packages/gephi-lite/src/core/Root.tsx
@@ -4,7 +4,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 
 import { ErrorComponent } from "../components/Error";
 import { MatomoProvider } from "../components/Matomo";
-import { DesignSystemPage } from "../designSystem/page";
+import DesignSystemPage from "../designSystem";
 import { NotFoundPage } from "../views/NotFoundPage";
 import { GraphPage } from "../views/graphPage";
 import { Initialize } from "./Initialize";
@@ -33,7 +33,8 @@ export const Root: FC = () => {
             <AtomsContextsRoot>
               <Initialize>
                 <Routes>
-                  <Route path="/design-system" element={<DesignSystemPage />} />
+                <Route path="/design-system" element={<DesignSystemPage />} />
+                  <Route path="/design-system/:page" element={<DesignSystemPage />} />
                   <Route path="/" element={<GraphPage />} />
                   {/* Error pages: */}
                   <Route path="*" element={<NotFoundPage />} />
