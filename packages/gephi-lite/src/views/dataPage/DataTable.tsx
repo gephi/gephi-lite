d5bb58baa8fdb6743a9ae7b1e561bc090c432294
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-06-24 17:43:25 +0200

    [gephi-lite] Improves Data table
    Details:
    - Moves sorting logic to dataTableAtom, so that it can be shared between
      the two tables when users search
    - Makes column labels truncate when needed
    - Drafts a dropdown menu on the right of column labels
    - Fixes various data table actions that were broken when given a state
      rather than a reducer
    - Fixes "Select all" button

--- packages/gephi-lite/src/views/dataPage/DataTable.tsx
+++ packages/gephi-lite/src/views/dataPage/DataTable.tsx
@@ -12,9 +12,11 @@ import {
 } from "@tanstack/react-table";
 import { VirtualItem, Virtualizer, useVirtualizer } from "@tanstack/react-virtual";
 import cx from "classnames";
-import { CSSProperties, FC, RefObject, useMemo, useRef } from "react";
+import { CSSProperties, FC, ReactNode, RefObject, useMemo, useRef } from "react";
+import { PiArrowDown, PiArrowUp, PiArrowsDownUp, PiDotsThreeVertical } from "react-icons/pi";
 import { ScrollSyncPane } from "react-scroll-sync";
 
+import Dropdown from "../../components/Dropdown";
 import { EdgeComponentById } from "../../components/Edge";
 import { NodeComponentById } from "../../components/Node";
 import {
@@ -26,6 +28,18 @@ import {
 } from "../../core/context/dataContexts";
 
 type ItemRow = { id: string; selected: boolean; data: ItemData };
+
+const ARROWS: Record<string, ReactNode> = {
+  asc: <PiArrowDown className="small ms-2" />,
+  desc: <PiArrowUp className="small ms-2" />,
+  both: <PiArrowsDownUp className="small ms-2" />,
+};
+
+const SPECIFIC_COLUMNS = {
+  id: "id",
+  selected: "selected",
+  preview: "preview",
+} as const;
 
 const getCommonPinningStyles = (column: Column<ItemRow>, isInHead?: boolean): CSSProperties => {
   const isPinned = column.getIsPinned();
@@ -105,9 +119,9 @@ const TableBody: FC<{
   );
 };
 
-const useDataTableColumns = () => {
+const useDataTableColumns = (itemIDs: string[]) => {
   const { type } = useDataTable();
-  const { toggle } = useSelectionActions();
+  const { toggle, select, unselect } = useSelectionActions();
   const { nodeFields, edgeFields } = useGraphDataset();
 
   const fields = useMemo(() => (type === "nodes" ? nodeFields : edgeFields), [edgeFields, nodeFields, type]);
@@ -116,14 +130,38 @@ const useDataTableColumns = () => {
     () => [
       // Agnostic columns;
       columnHelper.accessor((row) => row, {
-        id: "selected",
-        header: () => null,
-        size: 30,
+        id: SPECIFIC_COLUMNS.selected,
+        header: () => (
+          <div
+            className="text-center w-100"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              onChange={(e) =>
+                e.target.checked
+                  ? select({
+                      type,
+                      items: new Set(itemIDs),
+                    })
+                  : unselect({
+                      type,
+                      items: new Set(itemIDs),
+                    })
+              }
+            />
+          </div>
+        ),
+        size: 40,
         enableResizing: false,
         enablePinning: true,
         cell: (props) => (
-          <>
+          <div className="text-center w-100">
             <input
+              className="form-check-input"
               type="checkbox"
               checked={props.getValue().selected}
               onChange={() =>
@@ -133,11 +171,11 @@ const useDataTableColumns = () => {
                 })
               }
             />
-          </>
+          </div>
         ),
       }),
       columnHelper.display({
-        id: "preview",
+        id: SPECIFIC_COLUMNS.preview,
         header: "Preview",
         enablePinning: true,
         cell: (props) =>
@@ -148,7 +186,7 @@ const useDataTableColumns = () => {
           ),
       }),
       {
-        id: "id",
+        id: SPECIFIC_COLUMNS.id,
         accessorFn: ({ id }) => id,
         header: "ID",
         size: 60,
@@ -161,14 +199,14 @@ const useDataTableColumns = () => {
         header: () => fieldId,
       })),
     ],
-    [columnHelper, fields, toggle, type],
+    [columnHelper, fields, itemIDs, select, toggle, type, unselect],
   );
 
   return {
     columns,
     columnPinningState: {
       columnPinning: {
-        left: ["selected", "preview"],
+        left: [SPECIFIC_COLUMNS.selected, SPECIFIC_COLUMNS.preview],
       },
     },
   };
@@ -179,9 +217,9 @@ export const DataTable: FC<{ type: ItemType; itemIDs: string[] }> = ({ type, ite
   const { type: selectionType, items } = useSelection();
   const data = useMemo(() => (type === "nodes" ? nodeData : edgeData), [edgeData, nodeData, type]);
 
-  const { columnsState } = useDataTable();
-  const { updateColumnSizing, updateColumnSizingInfo } = useDataTableActions();
-  const { columns, columnPinningState } = useDataTableColumns();
+  const { dataTableState } = useDataTable();
+  const { updateColumnSizing, updateColumnSizingInfo, setSort } = useDataTableActions();
+  const { columns, columnPinningState } = useDataTableColumns(itemIDs);
 
   const rows = useMemo<ItemRow[]>(
     () => itemIDs.map((id) => ({ id, selected: selectionType === "nodes" && items.has(id), data: data[id] })),
@@ -195,13 +233,14 @@ export const DataTable: FC<{ type: ItemType; itemIDs: string[] }> = ({ type, ite
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     columnResizeMode: "onChange",
+    onSortingChange: setSort,
     defaultColumn: {
       maxSize: 400,
       minSize: 30,
       enableResizing: true,
     },
     state: {
-      ...columnsState,
+      ...dataTableState,
       ...columnPinningState,
     },
     onColumnSizingChange: updateColumnSizing,
@@ -225,30 +264,61 @@ export const DataTable: FC<{ type: ItemType; itemIDs: string[] }> = ({ type, ite
                   return (
                     <th key={header.id} style={{ ...getCommonPinningStyles(header.column, true) }}>
                       <div
-                        {...{
-                          className: header.column.getCanSort() ? "cursor-pointer select-none" : "",
-                          onClick: header.column.getToggleSortingHandler(),
-                        }}
+                        className={cx("content-wrapper", header.column.getCanSort() && "cursor-pointer select-none")}
+                        onClick={header.column.getToggleSortingHandler()}
                       >
-                        {flexRender(header.column.columnDef.header, header.getContext())}
-                        {{
-                          asc: " 🔼",
-                          desc: " 🔽",
-                        }[header.column.getIsSorted() as string] ?? null}
+                        <span className="column-title">
+                          {flexRender(header.column.columnDef.header, header.getContext())}
+                        </span>
+                        {ARROWS[header.column.getIsSorted() as string] ?? null}
+                        {header.id !== SPECIFIC_COLUMNS.selected && header.id !== SPECIFIC_COLUMNS.preview && (
+                          <Dropdown
+                            options={[
+                              {
+                                type: "option",
+                                label: <>Sort ascendingly {ARROWS.asc}</>,
+                                onClick: () => {
+                                  console.log("HUHUHU", header.id);
+                                  setSort([
+                                    {
+                                      id: header.id,
+                                      desc: false,
+                                    },
+                                  ]);
+                                },
+                              },
+                              {
+                                type: "option",
+                                label: <>Sort descendingly {ARROWS.desc}</>,
+                                onClick: () => {
+                                  console.log("HUHUHU", header.id);
+                                  setSort([
+                                    {
+                                      id: header.id,
+                                      desc: true,
+                                    },
+                                  ]);
+                                },
+                              },
+                            ]}
+                          >
+                            <button className="btn p-0">
+                              <PiDotsThreeVertical />
+                            </button>
+                          </Dropdown>
+                        )}
                         {header.column.getCanResize() && (
                           <div
-                            {...{
-                              className: `resizer ${header.column.getIsResizing() ? "is-resizing" : ""}`,
-                              onTouchStart: header.getResizeHandler(),
-                              onDoubleClick: () => header.column.resetSize(),
-                              onClick: (e) => {
-                                e.preventDefault();
-                                e.stopPropagation();
-                              },
-                              onMouseDown: (e) => {
-                                e.preventDefault();
-                                header.getResizeHandler()(e);
-                              },
+                            className={cx("resizer", header.column.getIsResizing() && "is-resizing")}
+                            onTouchStart={header.getResizeHandler()}
+                            onDoubleClick={() => header.column.resetSize()}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              e.stopPropagation();
+                            }}
+                            onMouseDown={(e) => {
+                              e.preventDefault();
+                              header.getResizeHandler()(e);
                             }}
                           />
                         )}
