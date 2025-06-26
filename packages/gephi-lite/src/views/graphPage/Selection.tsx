677bd5d8287de8754cace5cd464ca70684677240
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-06-26 17:46:16 +0200

    [gephi-lite] Plugs generic attributes rendering to selection panel

--- packages/gephi-lite/src/views/graphPage/Selection.tsx
+++ packages/gephi-lite/src/views/graphPage/Selection.tsx
@@ -1,6 +1,6 @@
-import { StaticDynamicItemData } from "@gephi/gephi-lite-sdk";
+import { FieldModel, Scalar, StaticDynamicItemData } from "@gephi/gephi-lite-sdk";
 import cx from "classnames";
-import { groupBy, isNil, toPairs } from "lodash";
+import { groupBy, toPairs } from "lodash";
 import { FC, ReactNode, useEffect, useMemo, useState } from "react";
 import AnimateHeight from "react-animate-height";
 import { useTranslation } from "react-i18next";
@@ -8,12 +8,12 @@ import { AiFillEdit } from "react-icons/ai";
 import { BiTargetLock } from "react-icons/bi";
 import { BsChevronDown, BsChevronUp, BsFillTrashFill, BsThreeDotsVertical } from "react-icons/bs";
 import { MdDeselect, MdFilterCenterFocus, MdSelectAll } from "react-icons/md";
-import ReactLinkify from "react-linkify";
 
 import ConfirmModal from "../../components//modals/ConfirmModal";
 import Dropdown from "../../components/Dropdown";
-import { EdgeComponent } from "../../components/data/Edge";
 import { InfiniteScroll } from "../../components/InfiniteScroll";
+import { RenderItemAttribute, RenderText } from "../../components/data/Attribute";
+import { EdgeComponent } from "../../components/data/Edge";
 import { NodeComponent } from "../../components/data/Node";
 import UpdateEdgeModal from "../../components/modals/edition/UpdateEdgeModal";
 import UpdateNodeModal from "../../components/modals/edition/UpdateNodeModal";
@@ -23,7 +23,6 @@ import {
   useFilteredGraph,
   useGraphDataset,
   useGraphDatasetActions,
-  usePreferences,
   useSelection,
   useSelectionActions,
   useVisualGetters,
@@ -33,11 +32,9 @@ import {
   mergeStaticDynamicData,
   staticDynamicAttributeLabel,
 } from "../../core/graph/dynamicAttributes";
-import { castScalarToModelValue } from "../../core/graph/fieldModel";
 import { EdgeRenderingData, NodeRenderingData } from "../../core/graph/types";
 import { useModal } from "../../core/modals";
 import { focusCameraOnEdge, focusCameraOnNode } from "../../core/sigma";
-import { DEFAULT_LINKIFY_PROPS } from "../../utils/url";
 
 function SelectedItem<
   T extends { type: "nodes"; data: NodeRenderingData } | { type: "edges"; data: EdgeRenderingData },
@@ -56,55 +53,43 @@ function SelectedItem<
 }) {
   const initiallyExpanded = useMemo(() => selectionSize === 1, [selectionSize]);
   const [expanded, setExpanded] = useState(initiallyExpanded);
-  const { locale } = usePreferences();
   const { t } = useTranslation();
   const { openModal } = useModal();
 
   const graphDataset = useGraphDataset();
+  const { edgeFields, nodeFields, fullGraph } = graphDataset;
+  const fields = useMemo(() => (type === "edges" ? edgeFields : nodeFields), [edgeFields, nodeFields, type]);
 
   const visualGetters = useVisualGetters();
   const filteredGraph = useFilteredGraph();
   const { deleteItems } = useGraphDatasetActions();
   const { select, unselect } = useSelectionActions();
 
-  const attributes = useMemo(() => {
-    const fields = type === "edges" ? graphDataset.edgeFields : graphDataset.nodeFields;
-
-    return [
+  const attributes = useMemo<{ label: ReactNode; value: Scalar; field?: FieldModel }[]>(
+    () => [
       { label: t(`graph.model.${type}-data.id`), value: id },
       ...fields.map((field) => ({
         label: staticDynamicAttributeLabel(field),
-        value: castScalarToModelValue(data.static[field.id], field),
+        field,
+        value: data.static[field.id],
       })),
       ...dynamicAttributes[type].map(({ field }) => ({
         label: staticDynamicAttributeLabel(field),
-        value: castScalarToModelValue(data.dynamic[field.id], field),
+        field,
+        value: data.dynamic[field.id],
       })),
       ...toPairs(renderingData).map(([field, value]) => ({ label: field, value })),
-    ].filter(({ value }) => !isNil(value));
-  }, [graphDataset.nodeFields, graphDataset.edgeFields, data, id, renderingData, t, type]);
+    ],
+    [data.dynamic, data.static, fields, id, renderingData, t, type],
+  );
 
   const item = getItemAttributes(type, id, filteredGraph, data, graphDataset, visualGetters);
   let content: ReactNode;
   if (type === "nodes") {
     content = <NodeComponent label={item.label} color={item.color} hidden={item.hidden} />;
   } else {
-    const source = getItemAttributes(
-      "nodes",
-      graphDataset.fullGraph.source(id),
-      filteredGraph,
-      data,
-      graphDataset,
-      visualGetters,
-    );
-    const target = getItemAttributes(
-      "nodes",
-      graphDataset.fullGraph.target(id),
-      filteredGraph,
-      data,
-      graphDataset,
-      visualGetters,
-    );
+    const source = getItemAttributes("nodes", fullGraph.source(id), filteredGraph, data, graphDataset, visualGetters);
+    const target = getItemAttributes("nodes", fullGraph.target(id), filteredGraph, data, graphDataset, visualGetters);
 
     content = <EdgeComponent {...item} source={source} target={target} className="mb-2" />;
   }
@@ -214,17 +199,15 @@ function SelectedItem<
       </h4>
       <AnimateHeight height={expanded ? "auto" : 0} className="position-relative" duration={400}>
         <ul className="list-unstyled small">
-          {attributes.map(({ label, value }) => (
-            <li key={label} className="overflow-hidden">
-              <div className="gl-container-muted-bg text-break gl-border gl-px-sm gl-py-xs">{label}</div>{" "}
+          {attributes.map((attribute, i) => (
+            <li key={i} className="overflow-hidden">
+              <div className="gl-container-muted-bg text-break gl-border gl-px-sm gl-py-xs">{attribute.label}</div>{" "}
               <div className="mb-1 text-break gl-px-sm">
-                <ReactLinkify {...DEFAULT_LINKIFY_PROPS}>
-                  {typeof value === "boolean"
-                    ? value.toString()
-                    : typeof value === "number"
-                      ? value.toLocaleString(locale)
-                      : value}
-                </ReactLinkify>
+                {attribute.field ? (
+                  <RenderItemAttribute value={attribute.value} field={attribute.field} />
+                ) : (
+                  <RenderText value={attribute.value + ""} />
+                )}
               </div>
             </li>
           ))}
