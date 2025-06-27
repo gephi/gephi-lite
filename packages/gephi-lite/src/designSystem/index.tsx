7edc831803c1a62fb94413ed7eed253b69730b76
Arthur Desaintjan <arthur@ouestware.com>
2025-06-27 17:07:31 +0200

    [design-system] mise en page

--- packages/gephi-lite/src/designSystem/index.tsx
+++ packages/gephi-lite/src/designSystem/index.tsx
@@ -16,7 +16,7 @@ const DesignSystemPage: FC = () => {
   const { page } = useParams();
 
   return (
-    <div className="container design-system-page">
+    <div className="container design-system-page d-flex flex-column gap-4">
       <h1>Gephi Lite Design System</h1>
 
       <ul className="d-flex flex-column">
