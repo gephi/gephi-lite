ccfb23f75967071c77b85c225ee4251d1e0977fa
Arthur Desaintjan <arthur@ouestware.com>
2025-07-03 10:16:38 +0200

    [design-system] corecting heading styles by reversing the names' sizes Now heading 1 > heading 3

--- packages/gephi-lite/src/components/modals/WelcomeModal.tsx
+++ packages/gephi-lite/src/components/modals/WelcomeModal.tsx
@@ -60,7 +60,7 @@ export const WelcomeModal: FC<ModalProps<unknown>> = ({ cancel, submit }) => {
     >
       <div className="row mb-3 position-relative">
         <div className="col-12 col-sm-6">
-          <h3 className=" gl-px-2 gl-heading-1">{t("welcome.open_recent")}</h3>
+          <h3 className=" gl-px-2 gl-heading-3">{t("welcome.open_recent")}</h3>
           {!!recentFiles.length && (
             <ul className="list-unstyled">
               {recentFiles
@@ -88,7 +88,7 @@ export const WelcomeModal: FC<ModalProps<unknown>> = ({ cancel, submit }) => {
           {!recentFiles.length && <p className="gl-text-muted">{t("welcome.no_recent")}</p>}
         </div>
         <div className="col-12 col-sm-6">
-          <h3 className="gl-px-2 gl-heading-1">{t("welcome.open_graph")}</h3>
+          <h3 className="gl-px-2 gl-heading-3">{t("welcome.open_graph")}</h3>
           <ul className="list-unstyled">
             <li className="mb-1">
               <button
@@ -128,7 +128,7 @@ export const WelcomeModal: FC<ModalProps<unknown>> = ({ cancel, submit }) => {
             </li>
           </ul>
           <br />
-          <h3 className="gl-px-2 gl-heading-1">{t("welcome.samples")}</h3>
+          <h3 className="gl-px-2 gl-heading-3">{t("welcome.samples")}</h3>
           <ul className="list-unstyled">
             {SAMPLES.map((sample) => (
               <li className="mb-1" key={sample}>
