18011f76d36def9c55d0e65129d38868c5f2bd9b
Benoit Simard <contact@bsimard.com>
2025-07-04 17:55:57 +0200

    [client] adding filter icon on graph summary

--- packages/gephi-lite/src/components/GraphSummary.tsx
+++ packages/gephi-lite/src/components/GraphSummary.tsx
@@ -4,7 +4,8 @@ import { capitalize } from "lodash";
 import { type FC, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
-import { useFilteredGraph, useGraphDataset, usePreferences } from "../core/context/dataContexts";
+import { useFilteredGraph, useFilters, useGraphDataset, usePreferences } from "../core/context/dataContexts";
+import { FiltersIcon, FiltersIconFill } from "./common-icons";
 
 const GraphStat: FC<{ className?: string; type: ItemType; current: number; total: number }> = ({
   className,
@@ -33,21 +34,29 @@ const GraphStat: FC<{ className?: string; type: ItemType; current: number; total
 
 export const GraphSummary: FC<{ className?: string }> = ({ className }) => {
   const { t } = useTranslation();
+  const filterState = useFilters();
   const filteredGraph = useFilteredGraph();
   const {
     metadata: { title, type },
     fullGraph,
   } = useGraphDataset();
 
+  const hasFilters = useMemo(
+    () => filterState.future.length + filterState.past.length,
+    [filterState.future, filterState.past],
+  );
+
   return (
     <div className={cx("graph-summary d-flex flex-column gl-gap-2", className)}>
       <div className="graph-title gl-px-2">{title}</div>
-      <div className="gl-px-2 gl-gap-x-2 d-flex flex-column">
+      <div className="gl-px-2 gl-gap-x-2 d-flex flex-column position-relative">
         <div className="d-flex flex-row flex-wrap gl-gap-x-2 gl-gap-y-3" style={{ lineHeight: 1.2 }}>
-          <GraphStat className="" type="nodes" current={filteredGraph.order} total={fullGraph.order} />
+          <GraphStat type="nodes" current={filteredGraph.order} total={fullGraph.order} />
           <GraphStat type="edges" current={filteredGraph.size} total={fullGraph.size} />
         </div>
         <span>{t(`graph.model.${type || "mixed"}_graph`)}</span>
+
+        {hasFilters && <FiltersIconFill style={{ left: "calc(100% - 1.5em)", top: 0, position: "absolute" }} />}
       </div>
     </div>
   );
