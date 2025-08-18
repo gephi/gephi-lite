ef4e964d4d71988d755cb1650f9b4a89291e0c1c
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-08-18 14:52:16 +0200

    [gephi-lite] Improves search indexation

--- packages/gephi-lite/src/core/search/utils.ts
+++ packages/gephi-lite/src/core/search/utils.ts
@@ -1,4 +1,3 @@
-import { ItemType } from "@gephi/gephi-lite-sdk";
 import { mapKeys } from "lodash";
 import MiniSearch from "minisearch";
 
@@ -11,33 +10,20 @@ export function getEmptySearchState(): SearchState {
   };
 }
 
-export function getItemLabel(itemType: ItemType, id: string, graph: SigmaGraph): string | undefined {
-  if (itemType === "nodes" ? !graph.hasNode(id) : !graph.hasEdge(id)) return undefined;
-
-  return (
-    (itemType === "nodes" ? graph.getNodeAttribute(id, "label") : graph.getEdgeAttribute(id, "label")) ?? undefined
-  );
-}
-
-export function nodeToDocument(graphDataset: GraphDataset, graph: SigmaGraph, id: string): Document {
+export function nodeToDocument(graphDataset: GraphDataset, _graph: SigmaGraph, id: string): Document {
   return {
     itemId: `nodes-${id}`,
     type: "nodes",
     id,
-    label: getItemLabel("nodes", id, graph),
-    // to avoid collision with our internal data, we prefix properties
-    // TODO: should we cast scalar to modelvalue ?
-    ...mapKeys(graphDataset.nodeData[id], (_value, key) => `prop_${key}`),
+    ...mapKeys(graphDataset.nodeData[id], (_value, key) => `prop_node_${key}`),
   };
 }
 
-export function edgeToDocument(graphDataset: GraphDataset, graph: SigmaGraph, id: string): Document {
+export function edgeToDocument(graphDataset: GraphDataset, _graph: SigmaGraph, id: string): Document {
   return {
     itemId: `edges-${id}`,
     type: "edges",
     id,
-    label: getItemLabel("edges", id, graph),
-    // to avoid collision with our internal data, we prefix properties
-    ...mapKeys(graphDataset.edgeData[id], (_value, key) => `prop_${key}`),
+    ...mapKeys(graphDataset.edgeData[id], (_value, key) => `prop_edge_${key}`),
   };
 }
