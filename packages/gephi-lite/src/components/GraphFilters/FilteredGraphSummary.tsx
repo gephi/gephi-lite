8b5f6476ae72846dcfe07a71b2d3a1cec00acfda
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-08-26 17:21:22 +0200

    [gephi-lite] Simplifies filters
    Details:
    - Removes "future" filters, and renames "past" filters as "filters"
    - Allows disabling each single filter
    - Removes current edited filter state, and makes all enabled filters
      editable all the time
    - Updates filters rendering (simple alternance between filter blocks and
      graph size blocks)

--- packages/gephi-lite/src/components/GraphFilters/FilteredGraphSummary.tsx
+++ packages/gephi-lite/src/components/GraphFilters/FilteredGraphSummary.tsx
@@ -1,18 +1,65 @@
-import { useReadAtom } from "@ouestware/atoms";
+import { isNil } from "lodash";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-import { filteredGraphsAtom } from "../../core/graph";
+import { useFilters, useFiltersActions, usePreferences } from "../../core/context/dataContexts";
+import { useFilteredGraphAt } from "../../core/graph";
+import { useModal } from "../../core/modals";
+import Dropdown from "../Dropdown";
+import { ThreeDotsVerticalIcon } from "../common-icons";
+import SelectFilterModal from "../modals/SelectFilterModal";
 
-export const FilteredGraphSummary: FC<{ filterIndex: number }> = ({ filterIndex }) => {
+export const FilteredGraphSummary: FC<{ filterIndex?: number }> = ({ filterIndex }) => {
   const { t } = useTranslation();
-  const filteredGraphs = useReadAtom(filteredGraphsAtom);
-  const relatedGraph = filteredGraphs[filterIndex]?.graph;
+  const { openModal } = useModal();
+  const { filters } = useFilters();
+  const { locale } = usePreferences();
+  const { disableFiltersFrom } = useFiltersActions();
+  const relatedGraph = useFilteredGraphAt(filterIndex || 0);
+  const nextFilterIndex = (filterIndex ?? -1) + 1;
+  const isVisibleGraph = !filters.slice(nextFilterIndex).some((filter) => !filter.disabled);
 
   return (
-    <div>
-      {relatedGraph.order} {t("graph.model.nodes", { count: relatedGraph.order })}, {relatedGraph.size}{" "}
-      {t("graph.model.edges", { count: relatedGraph.size })}
-    </div>
+    <section className="filter-graph">
+      <div className="gl-px-2">
+        {isNil(filterIndex) ? (
+          <div>{t("filters.full_graph")}</div>
+        ) : (
+          isVisibleGraph && <div>{t("filters.visible_graph")}</div>
+        )}
+        <div>
+          {relatedGraph.order.toLocaleString(locale)} {t("graph.model.nodes", { count: relatedGraph.order })},{" "}
+          {relatedGraph.size.toLocaleString(locale)} {t("graph.model.edges", { count: relatedGraph.size })}
+        </div>
+      </div>
+      <Dropdown
+        side="right"
+        options={[
+          {
+            label: t(`filters.insert`),
+            onClick: () => {
+              openModal({
+                component: SelectFilterModal,
+                arguments: {
+                  filterIndex: nextFilterIndex,
+                },
+              });
+            },
+          },
+          {
+            label: t(`filters.disable_following`),
+            title: t(`filters.disable_following_subtitle`),
+            disabled: isVisibleGraph,
+            onClick: () => {
+              disableFiltersFrom(nextFilterIndex);
+            },
+          },
+        ]}
+      >
+        <button className="gl-btn gl-btn-icon">
+          <ThreeDotsVerticalIcon />
+        </button>
+      </Dropdown>
+    </section>
   );
 };
