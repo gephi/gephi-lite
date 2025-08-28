5a65f1af32d444d724156fdb06c92da27921e904
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-08-28 17:22:33 +0200

    [gephi-lite] Refines 'Save workspace' modal

--- packages/gephi-lite/src/components/modals/save/SaveCloudFileForm.tsx
+++ packages/gephi-lite/src/components/modals/save/SaveCloudFileForm.tsx
@@ -1,5 +1,6 @@
 import { FC, useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { PiCloudArrowUp } from "react-icons/pi";
 
 import { useCloudProvider } from "../../../core/cloud/useCloudProvider";
 import { useFile, useFileActions } from "../../../core/context/dataContexts";
@@ -64,6 +65,7 @@ export const SaveCloudFileForm: FC<SaveCloudFileFormProps> = ({ id, onStatusChan
       {user ? (
         <form
           id={id}
+          className="h-100 d-flex flex-column gl-gap-1"
           onSubmit={(e) => {
             e.preventDefault();
             onSubmit();
@@ -71,46 +73,46 @@ export const SaveCloudFileForm: FC<SaveCloudFileFormProps> = ({ id, onStatusChan
         >
           {error && <p className="text-center text-danger">{t("graph.save.github.error").toString()}</p>}
 
-          <div className="mb-3">
-            <label htmlFor="filename" className="form-label">
-              {t("graph.save.github.field.filename").toString()}
-            </label>
+          <label htmlFor="filename" className="form-label">
+            {t("graph.save.github.field.filename").toString()}
+          </label>
+          <input
+            id="filename"
+            className="form-control mb-2"
+            type="string"
+            value={filename}
+            onChange={(e) => setFilename(e.target.value)}
+            required={true}
+          />
+
+          <label htmlFor="description" className="form-label">
+            {t("graph.save.github.field.description").toString()}
+          </label>
+          <textarea
+            id="description"
+            className="form-control mb-2 flex-grow-1"
+            value={description}
+            rows={3}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <div className="form-check mb-2">
             <input
-              id="filename"
-              className="form-control"
-              type="string"
-              value={filename}
-              onChange={(e) => setFilename(e.target.value)}
-              required={true}
+              className="form-check-input"
+              type="checkbox"
+              id="isPublic"
+              checked={isPublic}
+              onChange={(e) => setIsPublic(e.target.checked)}
             />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="description" className="form-label">
-              {t("graph.save.github.field.description").toString()}
+            <label className="form-check-label" htmlFor="isPublic">
+              {t("graph.save.github.field.isPublic").toString()}
             </label>
-            <textarea
-              id="description"
-              className="form-control"
-              value={description}
-              rows={3}
-              onChange={(e) => setDescription(e.target.value)}
-            />
           </div>
 
-          <div className="mb-3">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="isPublic"
-                checked={isPublic}
-                onChange={(e) => setIsPublic(e.target.checked)}
-              />
-              <label className="form-check-label" htmlFor="isPublic">
-                {t("graph.save.github.field.isPublic").toString()}
-              </label>
-            </div>
+          <div className="gl-gap-2 d-flex justify-content-end">
+            <button form="saveForm" className="gl-btn gl-btn-outline" disabled={loading}>
+              <PiCloudArrowUp /> {t("common.save").toString()}
+            </button>
           </div>
 
           {loading && <Loader />}
