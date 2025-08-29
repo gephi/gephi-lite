41a67b523610a7936ed2a8f003bf8605f3f8f164
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-08-29 17:51:05 +0200

    [gephi-lite] Softens Node and Edge components
    NodeComponentById and EdgeComponentById now renders dummy data when they
    can't find related items data.
    
    This happens particularly when the HMR fails, when we edit locally the
    locales.

--- packages/gephi-lite/src/components/data/Node.tsx
+++ packages/gephi-lite/src/components/data/Node.tsx
@@ -3,14 +3,22 @@ import { FC, ReactNode, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
 import { getItemAttributes } from "../../core/appearance/utils";
-import { useDynamicItemData, useFilteredGraph, useGraphDataset, useVisualGetters } from "../../core/context/dataContexts";
+import {
+  useDynamicItemData,
+  useFilteredGraph,
+  useGraphDataset,
+  useVisualGetters,
+} from "../../core/context/dataContexts";
 import { mergeStaticDynamicData } from "../../core/graph/dynamicAttributes";
 
 export const NodeComponent: FC<{ label: ReactNode; color: string; hidden?: boolean }> = ({ label, color, hidden }) => {
   const { t } = useTranslation();
   return (
     <div className="d-flex align-items-center mw-100">
-      <span className={cx(hidden ? "circle" : "disc gl-border", "me-1 flex-shrink-0 ")} style={{ backgroundColor: color }} />
+      <span
+        className={cx(hidden ? "circle" : "disc gl-border", "me-1 flex-shrink-0 ")}
+        style={{ backgroundColor: color }}
+      />
       <span className={cx(hidden && "text-muted", !label && "fst-italic", "flex-shrink-1 text-truncate")}>
         {label || t("selection.node_no_label")}
       </span>
@@ -26,16 +34,22 @@ export const NodeComponentById: FC<{ id: string }> = ({ id }) => {
 
   const data = useMemo(
     () =>
-      getItemAttributes(
-        "nodes",
-        id,
-        filteredGraph,
-        mergeStaticDynamicData(graphDataset.nodeData, dynamicItemData.dynamicNodeData)[id],
-        graphDataset,
-        visualGetters,
-      ),
+      filteredGraph.hasNode(id)
+        ? getItemAttributes(
+            "nodes",
+            id,
+            filteredGraph,
+            mergeStaticDynamicData(graphDataset.nodeData, dynamicItemData.dynamicNodeData)[id],
+            graphDataset,
+            visualGetters,
+          )
+        : null,
     [id, graphDataset, visualGetters, dynamicItemData, filteredGraph],
   );
 
-  return <NodeComponent {...data} />;
+  return data ? (
+    <NodeComponent {...data} />
+  ) : (
+    <NodeComponent label={<span className="fst-italic">?</span>} color="lightgrey" />
+  );
 };
