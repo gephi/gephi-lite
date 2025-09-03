491f80b98f6be9877824793131a5f1c192c9da9c
Paul Girard <paul@ouestware.com>
2025-09-03 17:13:18 +0200

    [Menu Panel] rollback to graph metadata form in a modal

--- packages/gephi-lite/src/components/GraphSummary.tsx
+++ packages/gephi-lite/src/components/GraphSummary.tsx
@@ -1,18 +1,13 @@
 import type { ItemType } from "@gephi/gephi-lite-sdk";
 import cx from "classnames";
 import { capitalize } from "lodash";
-import { type FC, useCallback, useEffect, useMemo, useState } from "react";
+import { type FC, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import TetherComponent from "react-tether";
 
-import {
-  useFilteredGraph,
-  useFilters,
-  useGraphDataset,
-  useGraphDatasetActions,
-  usePreferences,
-} from "../core/context/dataContexts";
-import { EditIcon, EditIconFill, FiltersIconFill } from "./common-icons";
+import { useFilteredGraph, useFilters, useGraphDataset, usePreferences } from "../core/context/dataContexts";
+import { useModal } from "../core/modals";
+import { EditIcon, FiltersIconFill } from "./common-icons";
+import { GraphMetadataModal } from "./modals/GraphMetadataModal";
 
 const GraphStat: FC<{ className?: string; type: ItemType; current: number; total: number }> = ({
   className,
@@ -39,116 +34,21 @@ const GraphStat: FC<{ className?: string; type: ItemType; current: number; total
   );
 };
 
-const GraphTitleEditable: FC<{ title?: string; save: (title?: string) => void }> = ({ title, save }) => {
-  const [tether, setTether] = useState<TetherComponent | null>(null);
-  const [mode, setMode] = useState<"edit" | "read">("read");
-  const [value, setValue] = useState(title);
-  const [textareaRef, setTextareaRef] = useState<HTMLElement | null>(null);
-
-  const submit = useCallback(() => {
-    save(value);
-    setMode("read");
-  }, [value, save]);
-
-  const cancel = useCallback(() => {
-    setValue(title);
-    setMode("read");
-  }, [title]);
-
-  useEffect(() => {
-    setValue(title);
-  }, [title]);
-
-  useEffect(() => {
-    textareaRef?.focus();
-  }, [textareaRef]);
-
-  // Handle interactions:
-  useEffect(() => {
-    const elementWrapper = tether?._elementNode;
-    const targetWrapper = tether?._targetNode;
-    const handleClickBody = (e: MouseEvent) => {
-      if (!elementWrapper?.current || !targetWrapper?.current) return;
-
-      const node = e.target as Node;
-      if (!elementWrapper.current.contains(node) && !targetWrapper?.current.contains(node)) {
-        cancel();
-      }
-    };
-
-    setTimeout(() => {
-      document.body.addEventListener("click", handleClickBody);
-    }, 0);
-    return () => {
-      document.body.removeEventListener("click", handleClickBody);
-    };
-  }, [tether, cancel]);
+const GraphTitle: FC<{ title?: string }> = ({ title }) => {
+  const { openModal } = useModal();
 
   return (
     <div className="graph-title d-flex" style={{ alignItems: "baseline" }}>
-      {mode === "read" ? (
-        <span className="flex-grow-1 gl-px-2 gl-text-wrap-anywhere"> {title}</span>
-      ) : (
-        <TetherComponent
-          ref={setTether}
-          attachment="top left"
-          targetAttachment="top left"
-          id="graph-title-edition"
-          constraints={[{ to: "scrollParent", attachment: "together", pin: true }]}
-          renderTarget={(ref) => (
-            <span ref={ref} className="flex-grow-1 gl-px-2 gl-text-wrap-anywhere">
-              {title}
-            </span>
-          )}
-          renderElement={(ref) => (
-            <div
-              ref={ref}
-              style={{
-                width: tether?._targetNode?.current?.clientWidth,
-              }}
-            >
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  submit();
-                }}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    e.preventDefault();
-                    submit();
-                  }
-                  if (e.key === "Escape") {
-                    cancel();
-                  }
-                  // to be able to do tab/enter to save the form
-                  if (e.key === "Tab") {
-                    e.preventDefault();
-                    document.getElementById("graph-title-btn")!.focus();
-                  }
-                }}
-              >
-                <textarea
-                  ref={setTextareaRef}
-                  className="form-control lh-1"
-                  style={{ paddingTop: "9px" }}
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-                />
-              </form>
-            </div>
-          )}
-        />
-      )}
+      <span className="flex-grow-1 gl-px-2 gl-text-wrap-anywhere"> {title}</span>
 
       <button
         id="graph-title-btn"
         onClick={() => {
-          if (mode === "read") setMode("edit");
-          else submit();
+          openModal({ component: GraphMetadataModal, arguments: {} });
         }}
-        className={cx("gl-btn gl-btn-icon", mode === "edit" && "gl-btn-fill")}
+        className={cx("gl-btn gl-btn-icon")}
       >
-        {mode === "read" ? <EditIcon /> : <EditIconFill />}
+        <EditIcon />
       </button>
     </div>
   );
@@ -158,21 +58,13 @@ export const GraphSummary: FC<{ className?: string }> = ({ className }) => {
   const { t } = useTranslation();
   const filterState = useFilters();
   const filteredGraph = useFilteredGraph();
-  const { setGraphMeta } = useGraphDatasetActions();
   const { metadata, fullGraph } = useGraphDataset();
 
   const hasFilters = useMemo(() => !!filterState.filters.length, [filterState.filters]);
 
-  const saveTitle = useCallback(
-    (title: string) => {
-      setGraphMeta({ ...metadata, title });
-    },
-    [setGraphMeta, metadata],
-  );
-
   return (
     <div className={cx("graph-summary d-flex flex-column gl-gap-2", className)}>
-      <GraphTitleEditable title={metadata.title} save={saveTitle} />
+      <GraphTitle title={metadata.title} />
       <div className="gl-px-2 gl-gap-x-2 d-flex flex-column position-relative">
         <div className="d-flex flex-row flex-wrap gl-gap-x-3 gl-gap-y-3" style={{ lineHeight: 1.2 }}>
           <GraphStat type="nodes" current={filteredGraph.order} total={fullGraph.order} />
