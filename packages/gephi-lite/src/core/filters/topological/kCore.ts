b92ee3ff53e272e7b977b039a0252fcd6a2ce58f
Paul Girard <paul@ouestware.com>
2025-09-03 11:34:00 +0200

    [Filters] kcore bug when self loops relates #259

--- packages/gephi-lite/src/core/filters/topological/kCore.ts
+++ packages/gephi-lite/src/core/filters/topological/kCore.ts
@@ -1,13 +1,17 @@
 import { kCore } from "graphology-cores";
 import { t } from "i18next";
 
-import { FilterNumberParameter, TopologicalFilterDefinition } from "../types";
+import { FilterBooleanParameter, FilterNumberParameter, TopologicalFilterDefinition } from "../types";
 
-export const buildKCoreFilterDefinition = (): TopologicalFilterDefinition<[FilterNumberParameter]> => ({
+export const buildKCoreFilterDefinition = (): TopologicalFilterDefinition<
+  [FilterNumberParameter, FilterBooleanParameter]
+> => ({
   type: "topological",
   id: "kCore",
   label: t("filters.topology.kCore.label"),
-  summary: ([core]) => t("filters.topology.kCore.summary", { core: core }),
+  summary: ([core, keepSelfLoops]) =>
+    `${t("filters.topology.kCore.summary", { core: core })} ${keepSelfLoops ? t("filters.topology.kCore.withSelfLoops") : t("filters.topology.kCore.withoutSelfLoops")}`,
+
   parameters: [
     {
       id: "core",
@@ -17,8 +21,31 @@ export const buildKCoreFilterDefinition = (): TopologicalFilterDefinition<[Filte
       defaultValue: 2,
       min: 1,
     },
+    {
+      id: "keepSelfLoops",
+      type: "boolean",
+      label: t("filters.topology.kCore.keepSelfLoop"),
+      required: false,
+      defaultValue: true,
+    },
   ],
-  filter([core], graph) {
-    return kCore(graph, core);
+  filter([core, keepSelfLoops], graph) {
+    if (graph.allowSelfLoops === false || graph.selfLoopCount === 0) return kCore(graph, core);
+    else {
+      const graphWithoutSelfLoop = graph.copy();
+      // remove self loops to calculate k-core
+      const selfLoops = graphWithoutSelfLoop.filterEdges((e) => graphWithoutSelfLoop.isSelfLoop(e));
+      selfLoops.forEach((e) => graphWithoutSelfLoop.dropEdge(e));
+      const kCoreResult = kCore(graphWithoutSelfLoop, core);
+
+      if (keepSelfLoops)
+        selfLoops.forEach((e) => {
+          if (graph.isDirected(e))
+            kCoreResult.addDirectedEdgeWithKey(e, graph.source(e), graph.target(e), graph.getEdgeAttributes(e));
+          else kCoreResult.addUndirectedEdgeWithKey(e, graph.source(e), graph.target(e), graph.getEdgeAttributes(e));
+        });
+
+      return kCoreResult;
+    }
   },
 });
