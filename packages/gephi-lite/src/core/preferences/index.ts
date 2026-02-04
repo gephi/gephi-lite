2fd72ec01a27034db6c66fc2ba8b46ad0d69dae4
Paul Girard <paul@ouestware.com>
2026-02-04 12:29:40 +0100

    [Palette] last used color palette saved/reused - last used palettes are stored in preferences atom/localstorage - when a new palette is needed we look in history if any present for same field (id+itemType) - we use the last seen palette rather than generating a new one - TODO: augment existing palette if new values

--- packages/gephi-lite/src/core/preferences/index.ts
+++ packages/gephi-lite/src/core/preferences/index.ts
@@ -1,14 +1,19 @@
+import { PartitionColor, RankingColor } from "@gephi/gephi-lite-sdk";
 import { Producer, atom, producerToAction } from "@ouestware/atoms";
 
 import { localStorage } from "../../utils/storage";
 import { Preferences } from "./types";
-import { getAppliedTheme, getCurrentPreferences, serializePreferences } from "./utils";
+import { getAppliedTheme, getCurrentPreferences, isSameField, serializePreferences } from "./utils";
+
+// Maximum number of color palette to keep in history
+// Gephi Lite will save up to this number of partitions and this number of rankings in localstorage
+const MAX_NB_SAVED_COLOR_SPECS = 20;
 
 /**
  * Producers:
  * **********
  */
-const changeLocale: Producer<Preferences, [string]> = (locale) => {
+const changeLocale: Producer<Preferences, [Preferences["locale"]]> = (locale) => {
   // save the new locale in the state
   return (preferences) => ({
     ...preferences,
@@ -23,6 +28,22 @@ const changeTheme: Producer<Preferences, [Preferences["theme"]]> = (theme) => {
   });
 };
 
+const newColorPaletteUsage: Producer<Preferences, [PartitionColor | RankingColor]> = (partitionRankingColor) => {
+  const paletteType = partitionRankingColor.type;
+  return (preferences) => ({
+    ...preferences,
+    colors: {
+      ...preferences.colors,
+      [paletteType]: [
+        partitionRankingColor,
+        // add the new spec at beginning of pref removing the previous spec on the same field if already there
+        ...(preferences.colors[paletteType]?.filter((p) => !isSameField(p.field, partitionRankingColor.field)) || []),
+        // cut the cache to max length set in config
+      ].slice(0, MAX_NB_SAVED_COLOR_SPECS),
+    },
+  });
+};
+
 /**
  * Public API:
  * ***********
@@ -32,6 +53,7 @@ export const preferencesAtom = atom<Preferences>(getCurrentPreferences());
 export const preferencesActions = {
   changeLocale: producerToAction(changeLocale, preferencesAtom),
   changeTheme: producerToAction(changeTheme, preferencesAtom),
+  newColorPaletteUsage: producerToAction(newColorPaletteUsage, preferencesAtom),
 };
 
 /**
