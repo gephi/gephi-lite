9ceea9b6c0483c61d7de6d8803827a29cd2ec139
Paul Girard <paul@ouestware.com>
2026-02-13 10:53:28 +0100

    [palette] expand feature

--- packages/gephi-lite/src/components/GraphAppearance/color/utils.ts
+++ packages/gephi-lite/src/components/GraphAppearance/color/utils.ts
@@ -1,5 +1,5 @@
 import iwanthue from "iwanthue";
-import { every } from "lodash";
+import { every, values as getValues } from "lodash";
 
 export function isColor(strColor: string): boolean {
   const s = new Option().style;
@@ -7,11 +7,17 @@ export function isColor(strColor: string): boolean {
   return s.color !== "";
 }
 
-export function getPalette(values: string[]): Record<string, string> {
+export function getPalette(values: string[], originalPalette?: Record<string, string | null>): Record<string, string> {
   if (every(values, (v) => isColor(v))) {
     return values.reduce((iter, v) => ({ ...iter, [v]: v }), {});
   } else {
-    const palette = iwanthue(values.length);
-    return values.reduce((iter, v, i) => ({ ...iter, [v]: palette[i] }), {});
+    const currentColors = getValues(originalPalette).filter((c) => c !== null);
+    const palette = iwanthue(values.length, { originalColorsToExpand: currentColors, colorSpace: "all" });
+    const newColors = palette.filter((c) => !currentColors.includes(c));
+    console.log(currentColors, palette, newColors);
+    return values.reduce(
+      (iter, v, i) => ({ ...iter, [v]: originalPalette && originalPalette[v] ? originalPalette[v] : newColors[i] }),
+      {},
+    );
   }
 }
