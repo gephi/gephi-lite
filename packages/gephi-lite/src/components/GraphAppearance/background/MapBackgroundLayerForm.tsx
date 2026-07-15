d6b1f446a780de492f0ae41df84fd7195043e30b
Benoit Simard <contact@bsimard.com>
2026-07-15 15:41:29 +0200

    [gephi-lite] display coordinates warning when setting map background (#323)

--- packages/gephi-lite/src/components/GraphAppearance/background/MapBackgroundLayerForm.tsx
+++ packages/gephi-lite/src/components/GraphAppearance/background/MapBackgroundLayerForm.tsx
@@ -3,8 +3,10 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
 import { useAppearance, useAppearanceActions, usePreferences } from "../../../core/context/dataContexts";
+import { EVENTS, useEventsContext } from "../../../core/context/eventsContext";
 import { useModal } from "../../../core/modals";
 import { getDefaultMapStyle } from "../../../utils/map-style";
+import MessageAlert from "../../MessageAlert";
 import { CodeEditorIcon } from "../../common-icons";
 import { MapStyleEditorModal } from "./MapStyleEditorModal";
 
@@ -14,6 +16,7 @@ export const MapBackgroundLayerForm: FC = () => {
   const { theme } = usePreferences();
   const { setBackgroundLayer } = useAppearanceActions();
   const { openModal } = useModal();
+  const { emitter } = useEventsContext();
 
   const mapLayer = backgroundLayer?.type === "map" ? backgroundLayer : null;
 
@@ -26,44 +29,62 @@ export const MapBackgroundLayerForm: FC = () => {
   const styleName = (currentStyle?.name as string) || t("appearance.background.map.maplibre.custom_style");
 
   return (
-    <div className="panel-block">
-      <h3>{t("appearance.background.map.title")}</h3>
+    <>
+      {/* TODO : this message can be visible only if no geo layout have been executed on the graph. */}
+      <MessageAlert
+        message={
+          <>
+            <p>{t("appearance.background.map.position_warning")}</p>
+            <button
+              className="gl-btn gl-btn-outline gl-btn-sm"
+              onClick={() => emitter.emit(EVENTS.openMenu, { menuId: `layout-geographic` })}
+            >
+              {t("appearance.background.map.open_geo_layout")}
+            </button>
+          </>
+        }
+        type="success"
+      />
 
       <div className="panel-block">
-        <label className="form-label">{t("appearance.background.map.maplibre.style")}</label>
-        <div className="d-flex align-items-center gl-gap-2">
-          <span className="flex-grow-1 text-ellipsis">
-            {currentStyle ? styleName : t("appearance.background.map.maplibre.style_default")}
-          </span>
-          <button
-            type="button"
-            className="gl-btn gl-btn-outline gl-btn-sm"
-            title={t("appearance.background.map.maplibre.edit_style")}
-            onClick={() =>
-              openModal({
-                component: MapStyleEditorModal,
-                arguments: {
-                  initialStyle: JSON.stringify(currentStyle || getDefaultMapStyle(theme), null, 2),
-                },
-                beforeSubmit: ({ style }) => setMapLayer({ style }),
-              })
-            }
-          >
-            <CodeEditorIcon className="me-1" />
-            {t("common.edit")}
-          </button>
-          {currentStyle && (
+        <h3>{t("appearance.background.map.title")}</h3>
+
+        <div className="panel-block">
+          <label className="form-label">{t("appearance.background.map.maplibre.style")}</label>
+          <div className="d-flex align-items-center gl-gap-2">
+            <span className="flex-grow-1 text-ellipsis">
+              {currentStyle ? styleName : t("appearance.background.map.maplibre.style_default")}
+            </span>
             <button
               type="button"
               className="gl-btn gl-btn-outline gl-btn-sm"
-              title={t("appearance.background.map.maplibre.reset_style")}
-              onClick={() => setMapLayer({ style: undefined })}
+              title={t("appearance.background.map.maplibre.edit_style")}
+              onClick={() =>
+                openModal({
+                  component: MapStyleEditorModal,
+                  arguments: {
+                    initialStyle: JSON.stringify(currentStyle || getDefaultMapStyle(theme), null, 2),
+                  },
+                  beforeSubmit: ({ style }) => setMapLayer({ style }),
+                })
+              }
             >
-              {t("common.reset")}
+              <CodeEditorIcon className="me-1" />
+              {t("common.edit")}
             </button>
-          )}
+            {currentStyle && (
+              <button
+                type="button"
+                className="gl-btn gl-btn-outline gl-btn-sm"
+                title={t("appearance.background.map.maplibre.reset_style")}
+                onClick={() => setMapLayer({ style: undefined })}
+              >
+                {t("common.reset")}
+              </button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
